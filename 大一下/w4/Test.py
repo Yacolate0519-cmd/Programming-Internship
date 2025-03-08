d5f31cce04f613fbb90463d9bae865b96b30794b
Yacolate <d1345490@o365.fcu.edu.tw>
2025-03-08 18:01:42 +0800

    Add FoodItem, Menu, Order, and FoodItemSystem classes; implement basic menu and order management functionality

--- 大一下/w4/Test.py
+++ 大一下/w4/Test.py
@@ -1,138 +1,231 @@
-class FoodItem:
-    def __init__(self , name , price):
-        self.name = name
-        self.price = price
-
-class Menu(FoodItem):
-    def __init__(self):
-        self.all_items = {}    
-
-    def show_menu(self):
-        print('--'*30)
-        print('餐點\t價錢')
-        print('--'*30)
-        for key , value in self.all_items.items():
-            print(key , '\t' , value)
-        print('--'*30)
-
-    def add_item(self , name , price):
-        if name not in self.all_items.keys():
-            self.all_items[name] = price
-        else:
-            print('Item already exist')
-
-    def remove_item(self , name):
-        if name in self.all_items.keys():
-            self.all_items.pop(name)
-
-    def change_itme_price(self , name , price):
-        self.all_items[name] = price
-
 import time
-class Order:
-    def __init__(self , name , items , time = time.strftime("%Y-%m-%d %H:%M:%S") , tableware  = 0, special_service = 0):
-        self.name = name 
-        self.items = items
-        self.time = time 
-        self.tableware = tableware
-        self.special_service = special_service
+import os
 
-class FoodOrderingSystem(Menu , Order):
+class FoodItem:
+    def __init__(self, name, category, price, size=None):
+        self.name = name
+        self.category = category
+        self.price = price
+        self.size = size
+
+class Menu:
     def __init__(self):
-        self.orderings = {}
+        self.all_items = {}
 
     def show_menu(self):
-        menu.show_menu()
+        print('--' * 30)
+        print(f'餐點\t種類\t價格\t大小')
+        print('--' * 30)
+        for name, item in self.all_items.items():
+            size = item.size if item.size else "N/A"
+            print(f'{item.name}\t{item.category}\t{item.price}\t{size}')
+        print('--' * 30)
 
-    def add_item(self , name , price):
-        menu.add_item(name , price)
-
-    def remove_item(self , name):
-        menu.remove_item(name)
-
-    def create_ordering(self , name , shoppingList):
-        new_order = Order(name , shoppingList)
-        self.orderings[new_order.name] = shoppingList 
-
-    def search_ordering(self , name):
-        if name in self.orderings:
-            print(f'顧客名稱: {name}')
-            print(f'Shopping Cart: {self.orderings[name]}')
-
-    def update_ordering(self , name , shoppingList):
-        if name in self.orderings:
-            self.orderings[name] = shoppingList
-
-    def remove_ordering(self , name):
-        self.orderings.pop(name)
-    
-    def show_all_ordering(self):
-        if not self.orderings:
-            print('無訂單')
+    def add_item(self, name, category, price, size=None):
+        if name not in self.all_items:
+            self.all_items[name] = FoodItem(name, category, price, size)
         else:
-            for key , value in self.orderings.items():
-                print('--'*30)
-                print(f'訂單客戶: {key}, 訂單: {value}')
-                print('--'*30)
+            print('已有此商品')
 
-import os 
+    def remove_item(self, name):
+        if name in self.all_items:
+            self.all_items.pop(name)
+            print(f'已移除 {name}')
+        else:  
+            print(f'找不到商品 {name}')
+        
+    def change_price(self, name, price):
+        if name in self.all_items:
+            price = int(price)
+            self.all_items[name].price = price
+            print(f'餐點: {name}, 價格更新為: {price}')
+        else:
+            print('查無此餐點')
+
+class Order:
+    order_count = 1
+    
+    def __init__(self, customer_name, items, tableware=0, special=0):
+        self.order_id = Order.order_count
+        Order.order_count += 1
+        self.customer_name = customer_name
+        self.items = items
+        self.time = time.strftime('%Y-%m-%d %H:%M:%S')
+        self.tableware = tableware
+        self.special = special
+
+    def calculate_total(self):
+        return sum(item.price for item in self.items)
+
+    def show_order(self):
+        print('--'*30)
+        print(f'訂單編號: {self.order_id}, 客戶: {self.customer_name}, 時間: {self.time}')
+        for item in self.items:
+            print(f'{item.name}\t${item.price}')
+        print(f'總價格: ${self.calculate_total()}')
+        print('--'*30)
+
+   
+
+class Customer:
+    def __init__(self, name, contact):
+        self.name = name
+        self.contact = contact
+        self.orders = []
+
+    def create_order(self, menu, shopping_list):
+        items = []
+        for name in shopping_list:
+            if name in menu.all_items:
+                items.append(menu.all_items[name])
+            else:
+                print(f'菜單中找不到 {name}')
+        if items:
+            new_order = Order(self.name, items)
+            self.orders.append(new_order)
+            print(f'訂單已建立，您的訂單編號為 {new_order.order_id}')
+            return new_order.order_id, new_order.calculate_total()
+        return None, 0
+
+class FoodOrderingSystem:
+    def __init__(self):
+        self.menu = Menu()
+        self.customers = {}
+        self.orders = {}
+    
+    def create_customer(self, name, contact):
+        if name in self.customers:
+            print('使用者已存在')
+        else:
+            self.customers[name] = Customer(name, contact)
+            print('客戶已新增')
+    
+    def find_customer(self , name):
+        if name in self.customers:
+            customer = self.customers[name]
+            print('--'*30)
+            print(f'顧客資訊: \n姓名: {customer.name}\n聯絡方式: {customer.contact}')
+            print('--'*30)
+        else:
+            print('查無此顧客')
+
+    def create_order(self, name, shopping_list):
+        if name not in self.customers:
+            print('請先建立客戶')
+            return
+        order_id, total_price = self.customers[name].create_order(self.menu, shopping_list)
+        if order_id:
+            self.orders[order_id] = self.customers[name].orders[-1]
+
+    def show_orders(self):
+        if not self.orders:
+            print('目前無訂單')
+        else:
+            for order in self.orders.values():
+                order.show_order()
+    
+    def find_order(self, order_id):
+        order_id = int(order_id)
+        if order_id in self.orders:
+            self.orders[order_id].show_order()
+        else:
+            print('查無此訂單')
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
+    system = FoodOrderingSystem()
+    menu = system.menu
 
-    menu = Menu()
-    menu.add_item('漢堡' , 100)
-    menu.add_item('薯條' , 40)
-    menu.add_item('可樂' , 45)
+    menu.add_item('漢堡', '主餐', 100)
+    menu.add_item('薯條', '點心', 40)
+    menu.add_item('可樂', '飲料', 45, '中杯')
 
-    while 1:
-        system = FoodOrderingSystem()
-        control = input('1.Admin\n2.Memeber\n3.Exit\n')
+    while True:
+        control = input("1.Admin(管理員)\n2.Member(會員)\n3.離開系統\n")
         if control == '1':
-            pass
-        elif control == '2':
-            while 1:
-                control = input('1.購物車\n2.Choose Ordering\n3.Send Ordering\n4.Exit\n')
+            clear()
+            while True:
+                control = input('1.Menu_manage\n2.Customer_manage\n3.Order_manage\n4.Exit\n')
                 if control == '1':
-                    pass
-                
-                elif control == '2':
+                    clear()
                     while 1:
-                        control = input('1.Show Menu\n2.Add_or_remove Item\n3.Ordering\n4.Exit\n')
+                        control = input('1.Show_Menu\n2.Add_or_remove\n3.Change_price\n4.Exit\n')
                         if control == '1':
                             menu.show_menu()
 
                         elif control == '2':
-                            control = input('1.Add_Item\n2.Remove_Item\n3.Update_item\n4.Search_Ordering\n5.quit\n') 
-                            if control == '1':
-                                menu.add_item()
+                            clear()
+                            while 1:
+                                control = input('1.Add_item\n2.Remove_item\n3.Exit\n')
+                                if control == '1':
+                                    name , category , price = input('請輸入餐點名稱、種類、價格: ').split()
+                                    size = input('若商品為飲料，請輸入大小，若不輸入，預設為N/A: ')
+                                    price = int(price)
+                                    menu.add_item(name , category , price , size if size else None)
+  
+                                elif control == '2':
+                                    name = input('輸入商品名稱: ')
+                                    menu.remove_item(name)
 
-                            elif control == '2':
-                                menu.remove_item()
+                                elif control == '3':
+                                    break
 
-                            elif control == '3':
-                                menu.update_ordering()
-
-                            elif control == '4':
-                                menu.search_ordering()
-
-                            elif control == '5':
-                                break   
-                        
                         elif control == '3':
-                            pass
+                            name , price = input('輸入商品以及更改後的價格: ').split()
+                            menu.change_price(name , price)
 
                         elif control == '4':
                             break
 
+                elif control == '2':
+                    name = input('輸入客戶姓名: ')
+                    '''
+                    查詢客戶資料
+                    '''
+                    system.find_customer(name)
 
                 elif control == '3':
-                    pass
-                
+                    clear()
+                    while 1:
+                        control = input('1.瀏覽所有訂單\n2.取消訂單\n3.Exit\n')
+                        if control == '1':
+                            system.show_orders()
+
+                        elif control == '2':
+                            id = int(input('輸入訂單編號: '))
+                            if id in system.orders:
+                                system.orders.pop(id)
+                            else:
+                                print('查無此訂單')
+
+                        elif control == '3':
+                            break
+
                 elif control == '4':
                     break
 
+        elif control == '2':
+            clear()
+            name, contact = input('請輸入姓名以及聯絡方式: ').split()
+            if name not in system.customers:
+                system.create_customer(name, contact)
+                while True:
+                    control = input('1.觀看菜單\n2.挑選商品\n3.送出訂單\n4.Exit\n')
+                    if control == '1':
+                        menu.show_menu()
+                    elif control == '2':
+                        shopping_list = input('輸入商品名稱: ').split()
+                        system.create_order(name, shopping_list)
+                    elif control == '3':
+                        print('訂單已送出')
+                    elif control == '4':
+                        break
+            else:
+                print('顧客已存在')
+
+            
         elif control == '3':
             break
     print('🍔感謝使用🍟')
